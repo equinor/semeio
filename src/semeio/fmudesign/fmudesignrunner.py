2216786cb70c5bb8f5f72c3ca31954f41085ba9d
Trine Alsos <tral@statoil.com>
2018-11-20 09:53:27 +0100

    fixes for issues #20 and #21, plus added bin/fmudesign

---
+++ src/semeio/fmudesign/fmudesignrunner.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""Script for generating a design matrix from config input"""
+
+from __future__ import division, print_function, absolute_import
+
+import argparse
+import sys
+import os.path
+
+from fmu.tools.sensitivities import DesignMatrix, excel2dict_design
+
+
+def _do_parse_args(args):
+
+    if args is None:
+        args = sys.argv[1:]
+    else:
+        args = args
+
+    usetxt = 'fmudesign ...'
+
+    parser = argparse.ArgumentParser(
+        description='Generate design matrix to be used with ert DESIGN2PARAMS',
+        usage=usetxt
+    )
+
+    # positional:
+    parser.add_argument('config',
+                        type=str,
+                        help=('Input design config filename '
+                              'on Excel format'))
+
+    parser.add_argument('--destination',
+                        dest='destination',
+                        default='../input/distributions/fmudesign.xlsx',
+                        type=str,
+                        help='Destination folder for design matrix')
+
+    if len(args) < 1:
+        parser.print_help()
+        print('QUIT')
+        raise SystemExit
+
+    args = parser.parse_args(args)
+    return args
+
+
+def main(args=None):
+    """fmudesign is a script that takes ..."""
+
+    args = _do_parse_args(args)
+
+    if isinstance(args.config, str):
+        if not os.path.isfile(args.config):
+            raise IOError('Input file does not exist')
+        input_dict = excel2dict_design(args.config)
+
+    design = DesignMatrix()
+
+    design.generate(input_dict)
+
+    design.to_xlsx(args.destination)
+
+
+if __name__ == '__main__':
+    main()
