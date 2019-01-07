577c82f49edf064c1e28b8600d7728512c17bdb5
Håvard Berland <havb@equinor.com>
2019-01-07 15:00:55 +0100

    add command line options for sheetnames
    The worksheets designinput, defaultvalues and general_input can
    be changed from the command line, individually.
    
    The existing permissive sheetname handling is conserved
    (generalinput in Excel is treated identically to general_input f.ex).
    
    Changed invocation of argparse, letting argparse print the
    usage instructions when wrong arguments are supplied.

--- src/semeio/fmudesign/fmudesignrunner.py
+++ src/semeio/fmudesign/fmudesignrunner.py
@@ -17,41 +17,70 @@ def _do_parse_args(args):
     else:
         args = args
 
-    usetxt = 'fmudesign ...'
-
     parser = argparse.ArgumentParser(
         description='Generate design matrix to be used with ert DESIGN2PARAMS',
-        usage=usetxt
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
     # positional:
     parser.add_argument('config',
                         type=str,
                         help=('Input design config filename '
-                              'on Excel format'))
-
+                              'in Excel format'))
     parser.add_argument('destination',
                         type=str,
-                        help='Destination folder for design matrix')
-
-    if len(args) < 2:
-        parser.print_help()
-        print('QUIT')
-        raise SystemExit
+                        help='Destination filename for design matrix',
+                        default='generateddesignmatrix.xlsx')
+    parser.add_argument('--designinput', type=str,
+                        help='Alternative sheetname for the ' + \
+                        'worksheet designinput',
+                        default='designinput')
+    parser.add_argument('--defaultvalues', type=str,
+                        help='Alternative sheetname for ' + \
+                        'worksheet defaultvalues',
+                        default='defaultvalues')
+    parser.add_argument('--general_input', type=str,
+                        help='Alternative sheetname for the' +\
+                        'worksheet general_input',
+                        default='general_input')
 
     args = parser.parse_args(args)
+
+    # Defaulted options should be reset to None, so that the other
+    # defaulting level inside _excel2dict can do its work.
+    if args.designinput == parser.get_default('designinput'):
+        args.designinput = None
+    if args.defaultvalues == parser.get_default('defaultvalues'):
+        args.defaultvalues = None
+    if args.general_input == parser.get_default('general_input'):
+        args.general_input = None
+
     return args
 
 
 def main(args=None):
-    """fmudesign is a script that takes ..."""
+    """fmudesign is a command line utility for generating design matrices
+
+    Wrapper for the the fmu.tools.sensitivities module"""
 
     args = _do_parse_args(args)
 
+    sheetnames = dict()
+    if args.designinput:
+        sheetnames['designinput'] = args.designinput
+    if args.defaultvalues:
+        sheetnames['defaultvalues'] = args.defaultvalues
+    if args.general_input:
+        sheetnames['general_input'] = args.general_input
+
+    if len(sheetnames):
+        print("Worksheets changed from default:")
+        print(sheetnames)
+
     if isinstance(args.config, str):
         if not os.path.isfile(args.config):
-            raise IOError('Input file does not exist')
-        input_dict = excel2dict_design(args.config)
+            raise IOError('Input file {} does not exist'.format(args.config))
+        input_dict = excel2dict_design(args.config, sheetnames)
 
     design = DesignMatrix()
 
