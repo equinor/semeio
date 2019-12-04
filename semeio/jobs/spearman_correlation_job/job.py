c7ab42ec4f2f0201d8db60fa3c1aff3e9e522405
Øyvind Eide <oyveid@equinor.com>
2019-12-04 16:03:17 +0100

    Add spearman correlation to observation data
    This adds job which calculates the spearman number for all observation
    keys and clusters the data accordingly.

---
+++ semeio/jobs/spearman_correlation_job/job.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+import itertools
+
+from ert_data.measured import MeasuredData
+from scipy.cluster.hierarchy import linkage, fcluster
+from semeio.jobs.correlated_observations_scaling.job import scaling_job
+
+
+def spearman_job(facade, threshold, dry_run):
+
+    observation_keys = [
+        facade.get_observation_key(nr) for nr, _ in enumerate(facade.get_observations())
+    ]
+
+    _spearman_correlation(facade, observation_keys, threshold, dry_run)
+
+
+def _spearman_correlation(facade, obs_keys, threshold, dry_run):
+    """
+    Collects data, performs scaling and applies scaling, assumes validated input.
+    """
+    measured_data = MeasuredData(facade, obs_keys)
+    measured_data.remove_failed_realizations()
+    measured_data.remove_inactive_observations()
+
+    simulated_data = measured_data.get_simulated_data()
+
+    correlation_matrix = _calculate_correlation_matrix(simulated_data)
+
+    clusters = _cluster_analysis(correlation_matrix, threshold)
+
+    columns = correlation_matrix.columns
+
+    # Here the clusters are joined with the key and data index
+    # to group the observations, the column level values are the column
+    # headers, where key_index is the observation key and data_index
+    # is a range.
+    data = list(
+        zip(
+            clusters,
+            columns.get_level_values(0),
+            columns.get_level_values("data_index"),
+        )
+    )
+
+    clustered_data = _cluster_data(data)
+
+    job_configs = _config_creation(clustered_data)
+
+    _output_clusters(clustered_data)
+
+    if not dry_run:
+        _run_scaling(facade, job_configs)
+
+
+def _output_clusters(clustered_data):
+    for cluster, val in clustered_data.items():
+        print("Cluster nr: {}, clustered data: {}".format(cluster, val))
+
+
+def _run_scaling(facade, job_configs):
+    for job in job_configs:
+        scaling_job(facade, job)
+
+
+def _cluster_data(data):
+    groups = {}
+    for (nr, key), cluster_group in itertools.groupby(sorted(data), key=lambda x: (x[0], x[1])):
+        if nr not in groups:
+            groups[nr] = {}
+        groups[nr].update({key: [index for _, _, index in cluster_group]})
+    return groups
+
+
+def _config_creation(clusters):
+    config = []
+    for cluster_nr, cluster in clusters.items():
+        config.append(
+            {
+                "CALCULATE_KEYS": {
+                    "keys": [{"key": key, "index": val} for key, val in cluster.items()]
+                }
+            }
+        )
+    return config
+
+
+def _calculate_correlation_matrix(data):
+    return data.corr(method="spearman")
+
+
+def _cluster_analysis(correlation_matrix, threshold):
+    a = linkage(correlation_matrix, "single")
+    return fcluster(a, threshold)
---
+++ tests/jobs/spearman_correlation_job/unit/test_spearman_job.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import sys
+
+import pytest
+from semeio.jobs.spearman_correlation_job import job as spearman
+
+if sys.version_info >= (3, 3):
+    from unittest.mock import Mock
+else:
+    from mock import Mock
+
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        ([{"CALCULATE_KEYS": {"keys": [{"key": "KEY_1", "index": [1]}]}}]),
+        ([{"CALCULATE_KEYS": {"keys": [{"key": "KEY_1", "index": [1, 2]}]}}]),
+        (
+            [
+                {
+                    "CALCULATE_KEYS": {
+                        "keys": [
+                            {"key": "KEY_1", "index": [1, 2]},
+                            {"key": "KEY_2", "index": [1, 2]},
+                        ]
+                    }
+                }
+            ]
+        ),
+        (
+            [
+                {"CALCULATE_KEYS": {"keys": [{"key": "KEY_1", "index": [1, 2]}]}},
+                {"CALCULATE_KEYS": {"keys": [{"key": "KEY_2", "index": [5, 6]}]}},
+            ]
+        ),
+    ],
+)
+def test_run_scaling(test_input, monkeypatch):
+    facade = Mock()
+    scal_job = Mock()
+    monkeypatch.setattr(spearman, "scaling_job", scal_job)
+    spearman._run_scaling(facade, test_input)
+    for input_config in test_input:
+        assert scal_job.any_call(input_config)
+    assert scal_job.call_count == len(test_input)
+
+
+@pytest.mark.parametrize(
+    "test_input,expected_result",
+    [
+        ([(1, "KEY_1", 1)], {1:{"KEY_1": [1]}}),
+        ([(1, "KEY_1", 1), (1, "KEY_1", 2)], {1: {"KEY_1": [1, 2]}}),
+        ([(1, "KEY_1", 1), (2, "KEY_2", 2)], {1: {"KEY_1": [1]}, 2: {"KEY_2": [2]}}),
+    ],
+)
+def test_make_clusters(test_input, expected_result):
+    result = spearman._cluster_data(test_input)
+    assert result == expected_result
+
+
+@pytest.mark.parametrize(
+    "test_input,expected_result",
+    [
+        (
+            {1: {"KEY_1": [1]}},
+            [{"CALCULATE_KEYS": {"keys": [{"key": "KEY_1", "index": [1]}]}}],
+        )
+    ],
+)
+def test_config_creation(test_input, expected_result):
+    result = spearman._config_creation(test_input)
+    assert result == expected_result
