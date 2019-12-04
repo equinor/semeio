13a31bd6e73c834eb6ea95bf51f1a17ab2d6d16e
Jonas G. Drange <jond@equinor.com>
2019-12-04 12:54:25 +0100

    Better describe package, and deploy it

--- setup.py
+++ setup.py
@@ -15,8 +15,10 @@ job_files = package_files("semeio/jobs/configs") + package_files("semeio/jobs/sc
 
 setup(
     name="semeio",
-    version="0.0.1",
+    use_scm_version={"write_to": "semeio/version.py"},
     author="Software Innovation Bergen, Equinor ASA",
+    author_email="fg_gpl@statoil.com",
+    url="https://github.com/equinor/semeio",
     description="Jobs and workflow jobs for Ert.",
     packages=[
         "semeio",
@@ -28,13 +30,27 @@ setup(
             "semeio_jobs = semeio.hook_implementations.jobs",
         ]
     },
+    license="GPL-3.0",
+    platforms="any",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
     install_requires=[
         "configsuite",
         "numpy",
         "pandas",
         "six",
     ],
-    setup_requires=["pytest-runner"],
+    setup_requires=["pytest-runner", "setuptools_scm"],
     tests_require=["pytest", "mock"],
     test_suite="tests",
     package_data={"": job_files},
