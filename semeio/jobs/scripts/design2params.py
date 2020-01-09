7db7217ec6a238f9679cb6b2a04b70d9a2220e31
Øystein Olai Heggen <ooh@equinor.com>
2020-01-09 08:34:37 +0100

    Port design2param

---
+++ semeio/jobs/scripts/design2params.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+#
+import argparse
+import logging
+import os
+import sys
+
+from semeio import valid_file
+from semeio.jobs.design2params import design2params
+
+description = """
+Reads a design matrix in XLSX-format and:
+ * Converts specified worksheet to txt-format and puts designmatrix.txt in RUNPATH
+ * Fetches values belonging to given ERT realization and *appends*
+   key-value pairs to parameters.txt
+ * Creates designparameters.txt which only contains variables taken from the design matrix, this
+   can be given to CUSTOM_KW
+Requires a matrix with column header as strings in topmost row in Excel.
+Row 2 in Excel must then correspond with the data you want for 'realization 0'
+Column 1 in Excel should contain the realization number, and will be ignored by this script.
+You must run this script as a FORWARD_MODEL from your ERT config, before
+you run DESIGN_KW.
+"""
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument(
+        "realization", type=int, help="Which realization",
+    )
+
+    parser.add_argument(
+        "xlsfilename", type=valid_file, help="Path to design-matrix (xlsx file)",
+    )
+
+    parser.add_argument(
+        "designsheetname", type=str, help="Design sheet name",
+    )
+
+    parser.add_argument(
+        "defaultssheetname",
+        type=str,
+        nargs="?",
+        help="Defaults sheet name",
+        default=None,
+    )
+
+    parser.add_argument(
+        "--parametersfilename",
+        "-p",
+        required=False,
+        default=design2params._PARAMETERS_TXT,
+        type=valid_file,
+    )
+
+    parser.add_argument(
+        "--log-level",
+        "-l",
+        required=False,
+        default="WARNING",
+        type=logging.getLevelName,
+    )
+
+    return parser
+
+
+def main(args):
+    parser = create_parser()
+    parsed_args = parser.parse_args(args)
+    design2params.run(
+        realization=parsed_args.realization,
+        xlsfilename=parsed_args.xlsfilename,
+        designsheetname=parsed_args.designsheetname,
+        defaultssheetname=parsed_args.defaultssheetname,
+        parametersfilename=parsed_args.parametersfilename,
+        log_level=parsed_args.log_level,
+    )
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ tests/jobs/design2params/test_design2params.py
@@ -0,0 +1,136 @@
+import filecmp
+import logging
+import os
+from distutils.dir_util import copy_tree
+
+import pytest
+
+from semeio.jobs.design2params import design2params
+
+
+@pytest.fixture
+def input_data(tmpdir):
+    data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+    copy_tree(data_dir, tmpdir.strpath)
+
+    cwd = os.getcwd()
+    tmpdir.chdir()
+
+    yield
+
+    os.chdir(cwd)
+
+
+@pytest.mark.usefixtures("input_data")
+@pytest.mark.parametrize(
+    "test_file, expected_file",
+    [
+        (design2params._PARAMETERS_TXT, "refparameters.txt"),
+        (design2params._DESIGN_MATRIX_TXT, "refdesignmatrix.txt"),
+        (design2params._DESIGN_PARAMETERS_TXT, "refdesignparameters.txt"),
+    ],
+)
+def test_run(test_file, expected_file):
+    design2params.run(
+        1,
+        "design_matrix.xlsx",
+        "DesignSheet01",
+        "DefaultValues",
+        design2params._PARAMETERS_TXT,
+        log_level=logging.DEBUG,
+    )
+
+    assert filecmp.cmp(test_file, expected_file)
+
+    with open(design2params._TARGET_FILE_TXT, "r") as status_file:
+        status = status_file.read()
+        assert status == "OK\n"
+
+
+@pytest.mark.usefixtures("input_data")
+@pytest.mark.parametrize(
+    "test_file, expected_file",
+    [
+        (design2params._PARAMETERS_TXT, "refparameters_w_default.txt"),
+        (design2params._DESIGN_MATRIX_TXT, "refdesignmatrix.txt"),
+        (design2params._DESIGN_PARAMETERS_TXT, "refdesignparameters_w_default.txt"),
+    ],
+)
+def test_run_with_default(test_file, expected_file):
+    design2params.run(
+        1,
+        "design_matrix_tampered_defaults.xlsx",
+        "DesignSheet01",
+        "DefaultValues",
+        design2params._PARAMETERS_TXT,
+        log_level=logging.DEBUG,
+    )
+
+    assert filecmp.cmp(test_file, expected_file)
+
+    with open(design2params._TARGET_FILE_TXT, "r") as status_file:
+        status = status_file.read()
+        assert status == "OK\n"
+
+
+@pytest.mark.usefixtures("input_data")
+@pytest.mark.parametrize("realization_id", [(0), (1), (15), (24)])
+def test_runs_different_reals_all_ok(realization_id):
+    design2params.run(
+        realization_id,
+        "design_matrix_tampered_defaults.xlsx",
+        "DesignSheet01",
+        "DefaultValues",
+        design2params._PARAMETERS_TXT,
+        log_level=logging.DEBUG,
+    )
+
+    with open(design2params._TARGET_FILE_TXT, "r") as status_file:
+        status = status_file.read()
+        assert status == "OK\n"
+
+
+def test_run_realization_not_exist(input_data):
+    with pytest.raises(SystemExit):
+        design2params.run(
+            1000,
+            "design_matrix.xlsx",
+            "DesignSheet01",
+            "DefaultValues",
+            design2params._PARAMETERS_TXT,
+            log_level=logging.DEBUG,
+        )
+
+
+def test_open_excel_file_not_existing(input_data):
+    with pytest.raises(SystemExit):
+        design2params._read_excel("not_existing.xlsx", "DesignSheet01")
+
+
+def test_open_excel_file_not_xls(input_data):
+    with pytest.raises(SystemExit):
+        design2params._read_excel("parameters.txt", "DesignSheet01")
+
+
+def test_open_excel_file_header_missing(input_data):
+    with pytest.raises(SystemExit):
+        design2params.run(
+            1,
+            "design_matrix_missing_header.xlsx",
+            "DesignSheet01",
+            "DefaultValues",
+            design2params._PARAMETERS_TXT,
+            log_level=logging.DEBUG,
+        )
+
+
+def test_open_excel_file_value_missing(input_data):
+    with pytest.raises(SystemExit):
+        design2params.run(
+            1,
+            "design_matrix_missing_value.xlsx",
+            "DesignSheet01",
+            "DefaultValues",
+            design2params._PARAMETERS_TXT,
+            log_level=logging.DEBUG,
+        )
