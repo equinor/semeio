a2e62393deabccecd18822dccac39e4cb42aa1d7
Sondre Sortland <sonso@equinor.com>
2020-01-13 10:35:58 +0100

    Integrate semeio with ERT plugin system

--- tests/hook_implementations/test_hook_implementations.py
+++ tests/hook_implementations/test_hook_implementations.py
@@ -11,5 +11,28 @@ from ert_shared.plugins.plugin_manager import ErtPluginManager
 class Test(unittest.TestCase):
     def test_hook_implementations(self):
         pm = ErtPluginManager(plugins=[semeio.hook_implementations.jobs])
-        self.assertDictEqual({}, pm.get_installable_jobs())
-        self.assertDictEqual({}, pm.get_installable_workflow_jobs())
+
+        expected_jobs = {
+            "DESIGN_KW": "semeio/jobs/config_jobs/DESIGN_KW",
+            "DESIGN2PARAMS": "semeio/jobs/config_jobs/DESIGN2PARAMS",
+            "STEA": "semeio/jobs/config_jobs/STEA",
+        }
+        installable_jobs = pm.get_installable_jobs()
+        for wf_name, wf_location in expected_jobs.items():
+            self.assertTrue(wf_name in installable_jobs)
+            self.assertTrue(installable_jobs[wf_name].endswith(wf_location))
+
+        self.assertSetEqual(set(installable_jobs.keys()), set(expected_jobs.keys()))
+
+        expected_workflow_jobs = {
+            "CORRELATED_OBSERVATIONS_SCALING": "semeio/jobs/config_workflow_jobs/CORRELATED_OBSERVATIONS_SCALING",
+            "SPEARMAN_CORRELATION": "semeio/jobs/config_workflow_jobs/SPEARMAN_CORRELATION",
+        }
+        installable_workflow_jobs = pm.get_installable_workflow_jobs()
+        for wf_name, wf_location in expected_workflow_jobs.items():
+            self.assertTrue(wf_name in installable_workflow_jobs)
+            self.assertTrue(installable_workflow_jobs[wf_name].endswith(wf_location))
+
+        self.assertSetEqual(
+            set(installable_workflow_jobs.keys()), set(expected_workflow_jobs.keys())
+        )
