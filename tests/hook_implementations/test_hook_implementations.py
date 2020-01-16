fad68901e9a5932808d664c0f7f129b5dd589b48
Lars Petter Øren Hauge <lpha@equinor.com>
2020-01-16 14:17:00 +0100

    Use pytest instead of unittest in test_hook

--- tests/hook_implementations/test_hook_implementations.py
+++ tests/hook_implementations/test_hook_implementations.py
@@ -1,5 +1,4 @@
 import sys
-import unittest
 
 import pytest
 
@@ -8,31 +7,28 @@ from ert_shared.plugins.plugin_manager import ErtPluginManager
 
 
 @pytest.mark.skipif(sys.version_info.major < 3, reason="requires python3")
-class Test(unittest.TestCase):
-    def test_hook_implementations(self):
-        pm = ErtPluginManager(plugins=[semeio.hook_implementations.jobs])
+def test_hook_implementations():
+    pm = ErtPluginManager(plugins=[semeio.hook_implementations.jobs])
 
-        expected_jobs = {
-            "DESIGN_KW": "semeio/jobs/config_jobs/DESIGN_KW",
-            "DESIGN2PARAMS": "semeio/jobs/config_jobs/DESIGN2PARAMS",
-            "STEA": "semeio/jobs/config_jobs/STEA",
-        }
-        installable_jobs = pm.get_installable_jobs()
-        for wf_name, wf_location in expected_jobs.items():
-            self.assertTrue(wf_name in installable_jobs)
-            self.assertTrue(installable_jobs[wf_name].endswith(wf_location))
+    expected_jobs = {
+        "DESIGN_KW": "semeio/jobs/config_jobs/DESIGN_KW",
+        "DESIGN2PARAMS": "semeio/jobs/config_jobs/DESIGN2PARAMS",
+        "STEA": "semeio/jobs/config_jobs/STEA",
+    }
+    installable_jobs = pm.get_installable_jobs()
+    for wf_name, wf_location in expected_jobs.items():
+        assert wf_name in installable_jobs
+        assert installable_jobs[wf_name].endswith(wf_location)
 
-        self.assertSetEqual(set(installable_jobs.keys()), set(expected_jobs.keys()))
+    assert set(installable_jobs.keys()) == set(expected_jobs.keys())
 
-        expected_workflow_jobs = {
-            "CORRELATED_OBSERVATIONS_SCALING": "semeio/jobs/config_workflow_jobs/CORRELATED_OBSERVATIONS_SCALING",
-            "SPEARMAN_CORRELATION": "semeio/jobs/config_workflow_jobs/SPEARMAN_CORRELATION",
-        }
-        installable_workflow_jobs = pm.get_installable_workflow_jobs()
-        for wf_name, wf_location in expected_workflow_jobs.items():
-            self.assertTrue(wf_name in installable_workflow_jobs)
-            self.assertTrue(installable_workflow_jobs[wf_name].endswith(wf_location))
+    expected_workflow_jobs = {
+        "CORRELATED_OBSERVATIONS_SCALING": "semeio/jobs/config_workflow_jobs/CORRELATED_OBSERVATIONS_SCALING",
+        "SPEARMAN_CORRELATION": "semeio/jobs/config_workflow_jobs/SPEARMAN_CORRELATION",
+    }
+    installable_workflow_jobs = pm.get_installable_workflow_jobs()
+    for wf_name, wf_location in expected_workflow_jobs.items():
+        assert wf_name in installable_workflow_jobs
+        assert installable_workflow_jobs[wf_name].endswith(wf_location)
 
-        self.assertSetEqual(
-            set(installable_workflow_jobs.keys()), set(expected_workflow_jobs.keys())
-        )
+    assert set(installable_workflow_jobs.keys()) == set(expected_workflow_jobs.keys())
