9c9bdcf6d3bc489634ae36916142e8a605da3565
Sondre Sortland <sondreso@users.noreply.github.com>
2020-02-03 08:09:07 +0100

    Add python 3.6 and 3.6 classifier list

--- setup.py
+++ setup.py
@@ -42,6 +42,8 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
