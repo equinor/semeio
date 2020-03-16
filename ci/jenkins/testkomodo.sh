dfdec3173bfa9db220132a1f9e83a445d9ce70b1
Åsmund Birkeland <aasmund.birkeland@gmail.com>
2020-03-16 15:08:46 +0100

    Move tests to new folder for komodo-testing

--- ci/jenkins/testkomodo.sh
+++ ci/jenkins/testkomodo.sh
@@ -16,7 +16,7 @@ mkdir $ENV
 python -m virtualenv --system-site-packages $ENV
 source $ENV/bin/activate
 python -m pip install -r test_requirements.txt
-
+ROOT_DIR=$(pwd)
 if [[ -z "${sha1// }" ]]; then
     EV=$(cat ${RELEASE_PATH}/${RELEASE_NAME} | grep "${PROJECT}:" -A2 | grep "version:")
     EV=($EV)    # split the string "version: vX.X.X"
@@ -24,10 +24,12 @@ if [[ -z "${sha1// }" ]]; then
     EV=${EV%"+py3"}
     echo "Using ${PROJECT} version ${EV}"
     $GIT checkout $EV
+    mkdir temp_tests
+    mv tests temp_tests
 
-    rm -rf !("tests"|"$ENV")
+    pushd temp_tests
 fi
 
 echo "running pytest"
-LIBRES_TEST_DATA_DIR=$(pwd)/libres/test-data python -m pytest \
+LIBRES_TEST_DATA_DIR=$ROOT_DIR/libres/test-data python -m pytest \
     --ignore="tests/test_formatting.py"
