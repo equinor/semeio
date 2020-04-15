c411e448dae9a5aacd4ab37695f65293bf7dc861
Markus Dregi <markusdregi@gmail.com>
2020-04-15 20:54:14 +0200

    Add misfit preprocessor observation config

---
+++ semeio/jobs/misfit_preprocessor/__init__.py
@@ -0,0 +1,10 @@
+from semeio.jobs.misfit_preprocessor.config import (
+    MisfitPreprocessorConfig,
+    assemble_config,
+)
+
+
+__all__ = (
+    "MisfitPreprocessorConfig",
+    "assemble_config",
+)
