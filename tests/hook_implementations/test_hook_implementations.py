0895b9b96aa4b88dbac3d8f0dc27994a2c13d483
Markus Dregi <markusdregi@gmail.com>
2020-05-27 20:04:11 +0200

    Implement misfit preprocessor script

--- tests/hook_implementations/test_hook_implementations.py
+++ tests/hook_implementations/test_hook_implementations.py
@@ -28,6 +28,7 @@ def test_hook_implementations():
         "CORRELATED_OBSERVATIONS_SCALING": "semeio/jobs/config_workflow_jobs/CORRELATED_OBSERVATIONS_SCALING",  # noqa
         "SPEARMAN_CORRELATION": "semeio/jobs/config_workflow_jobs/SPEARMAN_CORRELATION",
         "CSV_EXPORT2": "semeio/jobs/config_workflow_jobs/CSV_EXPORT2",
+        "MISFIT_PREPROCESSOR": "semeio/jobs/config_workflow_jobs/MISFIT_PREPROCESSOR",
     }
     installable_workflow_jobs = pm.get_installable_workflow_jobs()
     for wf_name, wf_location in expected_workflow_jobs.items():
