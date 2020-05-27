21b0b5ee254194f4f9a5250bef5953773f30e58c
Markus Dregi <markusdregi@gmail.com>
2020-05-27 19:37:18 +0200

    Implement misfit preprocessor job

---
+++ semeio/jobs/misfit_preprocessor/job.py
@@ -0,0 +1,32 @@
+from semeio.jobs.misfit_preprocessor.exceptions import ValidationError
+from semeio.jobs.misfit_preprocessor.config import (
+    SPEARMAN_CORRELATION,
+    assemble_config,
+)
+from semeio.jobs.spearman_correlation_job.job import spearman_job
+
+
+def run(misfit_preprocessor_config, measured_data):
+    config = assemble_config(misfit_preprocessor_config, measured_data)
+    if not config.valid:
+        raise ValidationError(
+            "Invalid configuration of misfit preprocessor", config.errors
+        )
+
+    config = config.snapshot
+    if config.clustering.method == SPEARMAN_CORRELATION:
+        sconfig = config.clustering.spearman_correlation
+        scaling_configs = spearman_job(
+            measured_data,
+            sconfig.fcluster.t,
+            criterion=sconfig.fcluster.criterion,
+            depth=sconfig.fcluster.depth,
+            method=sconfig.linkage.method,
+            metric=sconfig.linkage.metric,
+        )
+    else:
+        raise AssertionError(
+            "Unknown clustering method: {}".format(config.clustering.method)
+        )
+
+    return scaling_configs
