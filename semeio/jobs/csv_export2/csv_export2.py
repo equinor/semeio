3c218c2fa377ef74bf0a06ab79492801b5843086
Håvard Berland <havb@equinor.com>
2020-06-04 10:57:05 +0200

    Add name to EnsembleSet in csv_export2
    If not, a warning will be printed. Any name will do here
    and it will not be used anywhere in the output
    produced by csv_export2.

--- semeio/jobs/csv_export2/csv_export2.py
+++ semeio/jobs/csv_export2/csv_export2.py
@@ -7,7 +7,9 @@ def csv_exporter(runpathfile, time_index, outputfile, column_keys=None):
 
     The EnsembleSet is described by a runpathfile which must exists
     and point to realizations"""
-    ensemble_set = ensemble.EnsembleSet(runpathfile=runpathfile)
+    ensemble_set = ensemble.EnsembleSet(
+        name="ERT EnsembleSet for CSV_EXPORT2", runpathfile=runpathfile
+    )
     summary = ensemble_set.load_smry(time_index=time_index, column_keys=column_keys)
     parameters = ensemble_set.parameters
     summary_parameters = pd.merge(summary, parameters)
