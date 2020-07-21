38c013cfa40d8526835e64f444478624462575fb
Sondre Sortland <sonso@equinor.com>
2020-07-21 14:02:40 +0200

    Update testKomodo.sh to unified testing script

--- ci/jenkins/testkomodo.sh
+++ ci/jenkins/testkomodo.sh
@@ -1,36 +1,8 @@
-#!/bin/bash
-set -xe
-shopt -s extglob
-PROJECT=semeio
-RELEASE_PATH=${KOMODO_ROOT}/${RELEASE_NAME}
-GIT=${SDPSOFT}/bin/git
-source $KOMODO_ROOT/$RELEASE_NAME/enable
-export LD_LIBRARY_PATH=${RELEASE_PATH}/root/lib:${RELEASE_PATH}/root/lib64
 
-echo "fetch libres test data"
-$GIT clone https://github.com/equinor/libres.git
-
-echo "create virtualenv"
-ENV=testenv
-rm -rf $ENV
-mkdir $ENV
-python -m virtualenv --system-site-packages $ENV
-source $ENV/bin/activate
-ROOT_DIR=$(pwd)
-if [[ -z "${sha1// }" ]]; then
-    EV=$(cat ${RELEASE_PATH}/${RELEASE_NAME} | grep "${PROJECT}:" -A2 | grep "version:")
-    EV=($EV)    # split the string "version: vX.X.X"
-    EV=${EV[1]} # extract the version
-    EV=${EV%"+py3"}
-    echo "Using ${PROJECT} version ${EV}"
-    $GIT checkout $EV
-    mkdir temp_tests
-    mv tests temp_tests
-
-    pushd temp_tests
-fi
-python -m pip install -r  ${ROOT_DIR}/test_requirements.txt
-python -m pip install ${ROOT_DIR} --no-deps
-
-echo "running pytest"
-LIBRES_TEST_DATA_DIR=$ROOT_DIR/libres/test-data python -m pytest tests
+copy_test_files () {
+    cp -r $CI_SOURCE_ROOT/tests $CI_TEST_ROOT/tests
+    TMP_CLONE_DIR=$(mktemp -d)
+    git clone https://github.com/equinor/libres.git $TMP_CLONE_DIR/libres
+    mv $TMP_CLONE_DIR/libres/test-data $CI_TEST_ROOT/libres-test-data
+    export LIBRES_TEST_DATA_DIR=$CI_TEST_ROOT/libres-test-data
+}
