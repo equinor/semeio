3e7f405a7f62d91852092f9a7065821b3ddced04
Øyvind Eide <oyveid@equinor.com>
2020-09-07 10:33:18 +0200

    Change from old to new workflow registration for csv_export2

--- semeio/jobs/scripts/csv_export2.py
+++ semeio/jobs/scripts/csv_export2.py
@@ -3,6 +3,7 @@ import sys
 
 from res.enkf import ErtScript
 from semeio.jobs.csv_export2 import csv_export2
+from ert_shared.plugins.plugin_manager import hook_implementation
 
 
 class CsvExport2Job(ErtScript):  # pylint: disable=too-few-public-methods
@@ -29,5 +30,20 @@ def main(args):
     print("{} csv-export written to {}".format(args.time_index, args.outputfile))
 
 
+def csv_export_parser():
+    """Setup parser"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("runpathfile", type=str)
+    parser.add_argument("outputfile", type=str)
+    parser.add_argument("time_index", type=str, default="monthly")
+    parser.add_argument("column_keys", nargs="+", default=None)
+    return parser
+
+
 if __name__ == "__main__":
     main(sys.argv[1:])
+
+
+@hook_implementation
+def legacy_ertscript_workflow(config):
+    config.add_workflow(CsvExport2Job, "CSV_EXPORT2")
