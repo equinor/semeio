03b30752117a37036ef728f7b6fc6227fa5764e6
Åsmund Birkeland <musiv@equinor.com>
2020-09-17 10:29:30 +0200

    Set fixed tag for libres data

--- ci/jenkins/testkomodo.sh
+++ ci/jenkins/testkomodo.sh
@@ -2,7 +2,8 @@
 copy_test_files () {
     cp -r $CI_SOURCE_ROOT/tests $CI_TEST_ROOT/tests
     TMP_CLONE_DIR=$(mktemp -d)
-    git clone https://github.com/equinor/libres.git $TMP_CLONE_DIR/libres
+    # tag should be updated if newest test data should be used
+    git clone -b 5.0.1 https://github.com/equinor/libres.git $TMP_CLONE_DIR/libres
     mv $TMP_CLONE_DIR/libres/test-data $CI_TEST_ROOT/libres-test-data
     export LIBRES_TEST_DATA_DIR=$CI_TEST_ROOT/libres-test-data
 }
