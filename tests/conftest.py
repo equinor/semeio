2bf189fe7c8dbc5907295dd2412e1cac52f0519f
Øyvind Eide <eide.oyvind87@gmail.com>
2020-09-22 12:22:49 +0200

    Runs tests from shared tempdir to avoid storage leaks

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import gc
 import pytest
 from tests import legacy_test_data
@@ -17,3 +18,17 @@ def setup_tmpdir(tmpdir):
     # in semeio root dir at the end of the test run.
     # Should be deleted when we no longer use EnkFMain
     gc.collect()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def move_to_shared_tmp(tmp_path_factory):
+    """
+    We do this because when running some of the tests individually
+    they create a storage folder in the semeio root directory during
+    tear down. This places that storage folder in a shared tempdir
+    instead. Should be deleted when we no longer use EnkFMain
+    """
+    cwd = os.getcwd()
+    os.chdir(tmp_path_factory.mktemp("some_shared_path"))
+    yield
+    os.chdir(cwd)
