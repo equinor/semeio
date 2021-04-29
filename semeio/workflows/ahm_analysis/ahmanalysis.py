52c660fe26e87db42e4c76b71089328d45dc86ac
mrec <mrec@equinor.com>
2021-04-29 14:31:33 +0200

    correct ahm workflow description

--- semeio/workflows/ahm_analysis/ahmanalysis.py
+++ semeio/workflows/ahm_analysis/ahmanalysis.py
@@ -40,9 +40,9 @@ observations during the history matching process in ERT.
 
 Results will be exported in csv files which can be used in Webviz to perform
 analysis of the AHM through the AhmAnalysis plugin. CSV files will be stored
-under a folder named :code:`ert/output/storage/prior_name/reports/AhmAnalysisJob/`.
+under a folder named :code:`ert/output/storage/reports/prior_name/AhmAnalysisJob/`.
 prior_name will be the name of the case selected in current case on the ERT GUI
-or given as optional input to the workflow. Files generated are:
+(often :code:`default`)or given as optional input to the workflow. Files generated are:
 
  - One csv file per observation group with posterior distribution of the
    different parameters for each observation group included in the update.
