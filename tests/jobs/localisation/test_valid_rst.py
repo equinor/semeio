2a45ddd9c571ff8dac83cb5cd694006043c6d044
Oddvar Lia (ST MSU GEO) <olia@equinor.com>
2021-09-09 08:16:04 +0200

    Add documentation for first version

---
+++ tests/jobs/localisation/test_valid_rst.py
@@ -0,0 +1,12 @@
+from semeio.workflows.localisation.local_config_script import DESCRIPTION, EXAMPLES
+import rstcheck
+import pytest
+
+
+@pytest.mark.parametrize("rst_text", [DESCRIPTION, EXAMPLES])
+def test_valid_rst(rst_text):
+    """
+    Check that the documentation passed through the plugin system is
+    valid rst
+    """
+    assert not list(rstcheck.check(rst_text))
