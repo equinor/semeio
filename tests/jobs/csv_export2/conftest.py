11ff6befb2cc72c219bfc5bef54deb874748ab1d
Håvard Berland <havb@equinor.com>
2022-12-13 12:38:49 +0100

    Remove all traces of ert-statoil testdata
    * Rewrite csv_export tests to use a mocked set
    * Delete outdated/deprecated/superfluous test code in correlated
      observation scaling

--- tests/jobs/csv_export2/conftest.py
+++ tests/jobs/csv_export2/conftest.py
@@ -1,46 +1,8 @@
 import os
-import shutil
 
 import pytest
 
-TEST_DATA_DIR = os.environ.get("ERT_TESTDATA_ROOT")
-TEST_DATA_DIR_BERGEN = "/d/proj/bg/enkf/ErtTestData"
-TEST_DATA_DIR_STAVANGER = "/project/res-testdata/ErtTestData"
-
 NORNE_DIR = os.path.join(os.path.dirname(__file__), "../../test_data/norne")
-
-
-def find_available_test_data():
-    if TEST_DATA_DIR is None:
-        if os.path.isdir(TEST_DATA_DIR_STAVANGER):
-            return TEST_DATA_DIR_STAVANGER
-        if os.path.isdir(TEST_DATA_DIR_BERGEN):
-            return TEST_DATA_DIR_BERGEN
-
-    return TEST_DATA_DIR
-
-
-def has_equinor_test_data():
-    return os.path.isdir(find_available_test_data())
-
-
-def pytest_runtest_setup(item):
-    if item.get_closest_marker("equinor_test") and not has_equinor_test_data():
-        pytest.skip("Test requires Equinor data")
-
-
-@pytest.fixture()
-def ert_statoil_test_data(tmpdir):
-    cwd = os.getcwd()
-    tmpdir.chdir()
-    test_data_dir = os.path.join(find_available_test_data(), "ert-statoil", "spotfire")
-    shutil.copytree(test_data_dir, "test_data")
-    os.chdir(os.path.join("test_data"))
-
-    yield
-
-    os.chdir(cwd)
-    tmpdir.remove()
 
 
 def mock_norne_data(reals, iters, parameters=True):
