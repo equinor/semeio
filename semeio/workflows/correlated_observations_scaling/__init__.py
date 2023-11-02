1ccf4a01e6ef65ae953ce89097ac37328e393531
Øyvind Eide <eide.oyvind87@gmail.com>
2023-11-02 09:08:59 +0100

    Remove MisfitPreprocessor and assosiated workflows
    The observations have been moved from being in memory, to being
    stored in ert. This means that these workflows no longer have any
    effect. What was used has been reimplemted in ert.

--- semeio/workflows/correlated_observations_scaling/__init__.py
+++
@@ -1,3 +0,0 @@
-from semeio.workflows.correlated_observations_scaling.observation_scale_factor import (  # noqa
-    ObservationScaleFactor,
-)
--- semeio/workflows/misfit_preprocessor/__init__.py
+++
@@ -1,9 +0,0 @@
-from semeio.workflows.misfit_preprocessor.config import assemble_config
-from semeio.workflows.misfit_preprocessor.exceptions import ValidationError
-from semeio.workflows.misfit_preprocessor.job import run
-
-__all__ = (
-    "ValidationError",
-    "assemble_config",
-    "run",
-)
