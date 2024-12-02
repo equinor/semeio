f2cc08b42d8f094aad7a18da1207785c93e50897
Tommy Odland <todl@equinor.com>
2024-12-02 11:02:31 +0100

    Add function nearest_correlaction_matrix
    - remove function _is_positive_definite()
    - add symmetry and diagonal 1.0 to tests
    - update usage. replace _nearest_positive_definite() with nearest_correlation_matrix()
    - add back correlation matrix diagonal check in iman conover
    - add function nearest_correlation_matrix and tests

--- src/semeio/fmudesign/create_design.py
+++ src/semeio/fmudesign/create_design.py
@@ -6,8 +6,8 @@ from collections import OrderedDict
 from datetime import datetime
 from pathlib import Path
 
+import cvxpy as cp
 import numpy as np
-import numpy.linalg as la
 import pandas as pd
 import scipy
 from scipy.stats import qmc
@@ -17,42 +17,98 @@ from semeio.fmudesign import design_distributions as design_dist
 from semeio.fmudesign.iman_conover import ImanConover
 
 
-def _is_positive_definite(b_mat):
-    """Returns true when input is positive-definite, via Cholesky"""
-    try:
-        _ = la.cholesky(b_mat)
-        return True
-    except la.LinAlgError:
-        return False
+def nearest_correlation_matrix(matrix, *, weights=None, eps=1e-6, verbose=False):
+    """Returns the correlation matrix nearest to `matrix`, weighted elementwise
+    by `weights`.
 
+    Parameters
+    ----------
+    matrix : np.ndarray
+        The matrix that we want to find the nearest correlation matrix to.
+        A square 2-dimensional NumPy ndarray.
+    weights : np.ndarray or None, optional
+        An elementwise weighting matrix. A square 2-dimensional NumPy ndarray
+        that must have the same shape as `matrix`. The default is None.
+    eps : float, optional
+        Tolerance for the optimization solver. The default is 1e-6.
+    verbose : bool, optional
+        Whether to print information from the solver. The default is False.
 
-def _nearest_positive_definite(a_mat):
-    """Implementation taken from:
-    https://stackoverflow.com/questions/43238173/
-    python-convert-matrix-to-positive-semi-definite/43244194#43244194
+    Returns
+    -------
+    np.ndarray
+        The correlation matrix that is nearest to the input matrix.
+
+    Notes
+    -----
+    This function implements equation (3) in the paper "An Augmented Lagrangian
+    Dual Approach for the H-Weighted Nearest Correlation Matrix Problem" by
+    Houduo Qi and Defeng Sun.
+        http://www.personal.soton.ac.uk/hdqi/REPORTS/Cor_matrix_H.pdf
+    Another useful link is:
+        https://nhigham.com/2020/04/14/what-is-a-correlation-matrix/
+
+    Examples
+    --------
+    >>> X = np.array([[1, 1, 0],
+    ...               [1, 1, 1],
+    ...               [0, 1, 1]])
+    >>> nearest_correlation_matrix(X)
+    array([[1.        , 0.76068..., 0.15729...],
+           [0.76068..., 1.        , 0.76068...],
+           [0.15729..., 0.76068..., 1.        ]])
+    >>> H = np.array([[1,   0.5, 0.1],
+    ...               [0.5,   1, 0.5],
+    ...               [0.1, 0.5, 1]])
+    >>> nearest_correlation_matrix(X, weights=H)
+    array([[1.        , 0.94171..., 0.77365...],
+           [0.94171..., 1.        , 0.94171...],
+           [0.77365..., 0.94171..., 1.        ]])
     """
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("Input argument `matrix` must be np.ndarray.")
+    if not matrix.ndim == 2 and matrix.shape[0] == matrix.shape[1]:
+        raise ValueError("Input argument `matrix` must be square.")
 
-    b_mat = (a_mat + a_mat.T) / 2
-    _, s_mat, v_mat = la.svd(b_mat)
+    # Switch to notation used in the paper
+    G = matrix.copy()
+    H = np.ones_like(G) if weights is None else weights
 
-    h_mat = np.dot(v_mat.T, np.dot(np.diag(s_mat), v_mat))
+    if not isinstance(H, np.ndarray):
+        raise TypeError("Input argument `weights` must be np.ndarray.")
+    if not (H.shape == G.shape):
+        raise ValueError("Argument `weights` must have same shape as `matrix`.")
 
-    a2_mat = (b_mat + h_mat) / 2
+    # To constrain Y to be Positive Symmetric Definite (PSD), you need to
+    # either set PSD=True here, or add the special constraint 'Y >> 0'. See:
+    # https://www.cvxpy.org/tutorial/constraints/index.html#semidefinite-matrices
+    X = cp.Variable(shape=G.shape, PSD=True)
 
-    a3_mat = (a2_mat + a2_mat.T) / 2
+    # Objective and constraints for minimizing the weighted frobenius norm.
+    # This is equation (3) in the paper. We set (X - eps * I) >> 0 as an extra
+    # constraint. Mathematically this is not needed, but numerically it helps
+    # by nudging the solution slightly more, so the minimum eigenvalue is > 0.
+    objective = cp.norm(cp.multiply(H, X - G), "fro")
+    eps_identity = (eps / G.shape[0]) * 10
+    constraints = [cp.diag(X) == 1.0, (X - eps_identity * np.eye(G.shape[0])) >> 0]
 
-    if _is_positive_definite(a3_mat):
-        return a3_mat
+    # For solver options, see:
+    # https://www.cvxpy.org/tutorial/solvers/index.html#setting-solver-options
+    problem = cp.Problem(cp.Minimize(objective), constraints)
+    problem.solve(solver="SCS", verbose=verbose, eps=eps)
+    X = X.value.copy()  # Copy over solution
 
-    spacing = np.spacing(la.norm(a_mat))
-    identity = np.eye(a_mat.shape[0])
-    kiter = 1
-    while not _is_positive_definite(a3_mat):
-        mineig = np.min(np.real(la.eigvals(a3_mat)))
-        a3_mat += identity * (-mineig * kiter**2 + spacing)
-        kiter += 1
+    # We might get small eigenvalues due to numerics. Attempt to fix this by
+    # recursively calling the solver with smaller values of epsilon. This is
+    # an extra fail-safe that is very rarely triggered on actual data.
+    is_symmetric = np.allclose(X, X.T)
+    is_PD = np.linalg.eig(X).eigenvalues.min() > 0
+    if not (is_symmetric and is_PD) and (eps > 1e-14):
+        if verbose:
+            print(f"Recursively calling solver with eps := {eps} / 10")
+        return nearest_correlation_matrix(G, weights=H, eps=eps / 10, verbose=verbose)
 
-    return a3_mat
+    return X
 
 
 class DesignMatrix:
@@ -758,7 +814,9 @@ class MonteCarloSensitivity:
                     # Make correlation matrix symmetric by copying lower triangular part
                     correlations = np.triu(correlations.T, k=1) + np.tril(correlations)
 
-                    correlations = _nearest_positive_definite(correlations)
+                    correlations = nearest_correlation_matrix(
+                        correlations, weights=None, eps=1e-6, verbose=False
+                    )
 
                     sampler = qmc.LatinHypercube(
                         d=len(multivariate_parameters), seed=rng
