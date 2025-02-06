90c83778049eb4a23bfc189642e01d3882e903bb
Yngve S. Kristiansen <ynk@equinor.com>
2025-02-06 10:27:23 +0100

    Remove SemeioScript

--- src/semeio/communication/__init__.py
+++
@@ -1,4 +0,0 @@
-from semeio.communication.reporter import FileReporter
-from semeio.communication.semeio_script import SEMEIOSCRIPT_LOG_FILE, SemeioScript
-
-__all__ = ("FileReporter", "SemeioScript", "SEMEIOSCRIPT_LOG_FILE")
