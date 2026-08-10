2439a16e34d61f0fd2054759e3b93d51e0f90a7f
eq-sandkav <skav@equinor.com>
2026-08-10 12:31:08 +0200

    Delete unused fixtures

--- tests/workflows/conftest.py
+++ tests/workflows/conftest.py
@@ -1,7 +1,3 @@
-import os
-import pathlib
-import shutil
-
 import pytest
 from ert.config import ErtConfig
 
@@ -11,15 +7,3 @@ def snake_oil_config(
     copy_snake_oil_case_storage,
 ):  # pylint: disable=unused-argument
     return ErtConfig.from_file("snake_oil.ert")
-
-
-@pytest.fixture
-def setup_poly_ert(tmpdir, test_data_root):
-    cwd = pathlib.Path.cwd()
-    tmpdir.chdir()
-    test_data_dir = os.path.join(test_data_root, "poly_normal")
-    shutil.copytree(test_data_dir, "test_data")
-    os.chdir(os.path.join("test_data"))
-
-    yield
-    os.chdir(cwd)
