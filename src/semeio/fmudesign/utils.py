46d0ea0990d9e40d6ff3fce8eedea1a70b836287
eq-sandkav <skav@equinor.com>
2026-08-27 12:41:44 +0200

    SQUASHME: reorder precedence of path resolving

--- src/semeio/fmudesign/utils.py
+++ src/semeio/fmudesign/utils.py
@@ -262,15 +262,15 @@ def _is_int(teststring: str) -> bool:
 
 
 def resolve_path(target: str | None, base_file: str = "") -> str | None:
-    """Try to resolve the path of potential file 'target' as path either from cwd or
-    relative to 'base_file'."""
+    """Try to resolve the path of potential file 'target' as path either relative to
+    'base_file' or cwd."""
     if target is None:
         return None
 
-    if (path := Path(target)).is_file():
-        return str(path.resolve())
-
     if (relative_path := Path(base_file).parent / target).is_file():
         return str(relative_path.resolve())
+
+    if (path := Path(target)).is_file():
+        return str(path.resolve())
 
     return target
