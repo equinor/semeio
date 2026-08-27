6a78d22a17f9df9ce7b1bb9870bb240054092b94
eq-sandkav <skav@equinor.com>
2026-08-27 12:26:59 +0200

    Speed up test
    This test used to create a subprocess for each entry point, which
    made every test case use >4seconds, totalling in >30sec total time.
    
    The new solution will run all entry points in the same pytest
    process, and will dynamically detect all semeio entry points and
    assert that these match with the ones we expect to exist.

--- tests/test_console_scripts.py
+++ tests/test_console_scripts.py
@@ -1,24 +1,34 @@
+from importlib.metadata import entry_points
+
 import pytest
+
+EXPECTED_ENTRYPOINTS = {
+    "csv_export2",
+    "overburden_timeshift",
+    "design2params",
+    "fmudesign",
+    "gendata_rft",
+    "design_kw",
+    "fm_pyscal",
+    "replace_string",
+}
 
 
 @pytest.mark.integration_test
-@pytest.mark.script_launch_mode("subprocess")
-@pytest.mark.parametrize(
-    "entry_point",
-    [
-        "csv_export2",
-        "overburden_timeshift",
-        "design2params",
-        "gendata_rft",
-        "design_kw",
-        "fm_pyscal",
-    ],
-)
-def test_console_scripts_help(script_runner, entry_point):
-    """
-    This test is a quick test that the scripts are installed correctly,
-    by checking that we call call --help (which all these have). Semeio
-    must be installed for them to pass.
-    """
-    ret = script_runner.run([entry_point, "--help"])
-    assert ret.success
+def test_that_console_scripts_are_installed_and_their_help_command_returns_exit_code_0(
+    monkeypatch,
+):
+    semeio_entry_points = [
+        e
+        for e in iter(entry_points(group="console_scripts"))
+        if e.value.startswith("semeio.")
+    ]
+
+    semeio_entry_point_names = {e.name for e in semeio_entry_points}
+    assert semeio_entry_point_names == EXPECTED_ENTRYPOINTS
+
+    for entrypoint in semeio_entry_points:
+        func = entrypoint.load()
+        monkeypatch.setattr("sys.argv", [entrypoint.name, "--help"])
+        with pytest.raises(SystemExit, match="0"):
+            func()
